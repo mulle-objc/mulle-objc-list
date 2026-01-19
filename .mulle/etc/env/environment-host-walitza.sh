fbcf8112fc7c13092f5f26dc198da9050320c903
Nat! <nat@mulle-kybernetik.com>
2026-01-19 01:05:54 +0100

    fix stuff for github CI

--- .mulle/etc/env/environment-host-walitza.sh
+++ .mulle/etc/env/environment-host-walitza.sh
@@ -1,24 +1,24 @@
 #
 #
 #
-export MULLE_FETCH_SEARCH_PATH=":/Volumes/Source/srcO/mulle-cc:/Volumes/Source/srcO/mulle-c:/Volumes/Source/srcO/mulle-core:/Volumes/Source/srcO/mulle-concurrent:/Volumes/Source/srcO/mulle-objc:/Volumes/Source/srcO/mulle-objc-ports:/Volumes/Source/srcO/MulleFoundation:/Volumes/Source/srcO/MulleWeb:/Volumes/Source/srcO/MulleScion:/Volumes/Source/srcO/mulle-gfx:/Volumes/Source/srcO/MulleUI:/Volumes/Source/srcC/hdt_master/Frameworks:/Volumes/Source/srcM/MulleEOF/MulleEOControl:/Volumes/Source/srcM/MulleEOF/MulleEOAccess:/Volumes/Source/srcM/MulleEOF/MulleEOInterface:/Volumes/Source/srcM/MulleEOF/Adaptor:${MULLE_FETCH_SEARCH_PATH}"
+export MULLE_FETCH_SEARCH_PATH=":/Volumes/Source/srcO/mulle-c:/Volumes/Source/srcO/mulle-core:/Volumes/Source/srcO/mulle-concurrent:/Volumes/Source/srcO/mulle-objc:/Volumes/Source/srcO/mulle-gfx:/Volumes/Source/srcO/MulleFoundation:/Volumes/Source/srcO/MulleWeb:/Volumes/Source/srcO/MulleScion:/Volumes/Source/srcO/MulleUI:/Volumes/Source/srcC/hdt_master/Frameworks:/Volumes/Source/srcM/MulleEOF/MulleEOControl:/Volumes/Source/srcM/MulleEOF/MulleEOAccess:/Volumes/Source/srcM/MulleEOF/MulleEOInterface:/Volumes/Source/srcM/MulleEOF/Adaptor:/Volumes/Source/srcO/mulle-cc:/Volumes/Source/srcO/mulle-objc-ports:${MULLE_FETCH_SEARCH_PATH}"
 
 
 #
 #
 #
-export MULLE_SOURCETREE_STASH_DIR="${HOME}/.mulle/var/${PROJECT_NAME}-F8BD6E8E6348/stash"
+export MULLE_SOURCETREE_STASH_DIR="${HOME}/.mulle/var/cache/sde/${PROJECT_NAME}-${MULLE_VIRTUAL_ROOT_ID}/stash"
 
 
 #
 #
 #
-export DEPENDENCY_DIR="${HOME}/.mulle/var/${PROJECT_NAME}-F8BD6E8E6348/dependency"
+export DEPENDENCY_DIR="${HOME}/.mulle/var/cache/sde/${PROJECT_NAME}-${MULLE_VIRTUAL_ROOT_ID}/dependency"
 
 
 #
 #
 #
-export KITCHEN_DIR="${HOME}/.mulle/var/${PROJECT_NAME}-F8BD6E8E6348/kitchen"
+export KITCHEN_DIR="${HOME}/.mulle/var/cache/sde/${PROJECT_NAME}-${MULLE_VIRTUAL_ROOT_ID}/kitchen"
 
 
